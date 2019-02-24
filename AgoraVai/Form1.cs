76bfff9b454e156ea4ca8081f62bc064a51056a6
moisesrodriguesdev <moisesabreurodrigues@gmail.com>
2019-02-24 17:42:26 -0300

    windowsform basico usando conceitos de OO

---
+++ AgoraVai/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgoraVai
+{
+    public partial class Form1 : Form
+    {
+        Moto motoInstancia = new Moto();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            motoInstancia.marca = textMarca.Text;
+            motoInstancia.modelo = textModelo.Text;
+            motoInstancia.cor = textCor.Text;
+            motoInstancia.marcha = Convert.ToInt32(textMarcha.Text);
+
+            label9.Text = motoInstancia.marca;
+            label10.Text = motoInstancia.modelo;
+            label11.Text = motoInstancia.cor;
+            label15.Text = Convert.ToString(motoInstancia.marcha);
+     
+
+        }
+
+        private void aumentarMarcha_Click(object sender, EventArgs e)
+        {
+            motoInstancia.marchaAcima();
+            label15.Text =  Convert.ToString(motoInstancia.marcha);
+
+        }
+
+        private void DiminuirMarcha_Click(object sender, EventArgs e)
+        {
+            motoInstancia.marchaAbaixo();
+            label15.Text = Convert.ToString(motoInstancia.marcha);
+        }
+
+        private void ligar_Click(object sender, EventArgs e)
+        {
+            motoInstancia.ligar();
+            if(motoInstancia.ligada == true)
+            {
+                label14.Text = "Sim";
+            }
+        }
+
+        private void desligar_Click(object sender, EventArgs e)
+        {
+            motoInstancia.desligar();
+            if (motoInstancia.ligada == false)
+            {
+                label14.Text = "Não";
+            }
+        }
+    }
+}
---
+++ AgoraVai/Moto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgoraVai
+{
+    class Moto
+    {
+        public string marca;
+        public string modelo;
+        public string cor;
+        public int marcha;
+        public bool ligada;
+
+
+        public void definir(string marca, string modelo, string cor, int marcha)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.cor = cor;
+            this.marcha = marcha;
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("***************** Especificaões da Moto *****************");
+            Console.WriteLine("Marca: " + marca);
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Cor " + cor);
+            switch (marcha)
+            {
+                case 0:
+                    Console.WriteLine("Marcha está o Neutro");
+                    break;
+                case 1:
+                    Console.WriteLine("Marcha está engatada na 1°");
+                    break;
+                case 2:
+                    Console.WriteLine("Marcha está engatada na 2°");
+                    break;
+                case 3:
+                    Console.WriteLine("Marcha está engatada na 3°");
+                    break;
+            }
+            Console.WriteLine("Está ligada:" + ligada);
+        }
+
+        public void ligar()
+        {
+            ligada = true;
+        }
+        public void desligar()
+        {
+            ligada = false;
+        }
+        public void marchaAcima()
+        {
+            if (marcha >= 5)
+            {
+                Console.WriteLine("Limite maximo de marchas, troca por uma marcha abaixo");
+            }
+            else
+            {
+                marcha++;
+            }
+        }
+        public void marchaAbaixo()
+        {
+            if (marcha <= 0)
+            {
+                Console.WriteLine("Limite minimo de marchas, troque por uma acima");
+            }
+            else
+            {
+                marcha--;
+            }
+        }
+    }
+}
